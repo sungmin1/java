88a10474fe108b2ddd1cb20d91afe2cd3eea2d20
minsoo <minsoo@DESKTOP-8VRI6D3>
2019-05-17 10:39:16 +0900

    20190517

---
+++ chap02/src/chap02/VariableExample.java
@@ -0,0 +1,11 @@
+package chap02;
+
+public class VariableExample {
+	public static void main(String[] args) {
+		int value = 10;
+		
+		int result = value + 10;
+		
+		System.out.println(result);
+	}
+}
